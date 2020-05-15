b8de356116e62f9b57f183e08beda47c0c948ac2
karol-202 <karoljurski1@gmail.com>
2020-05-15 15:38:33 +0200

    Prepare web-canvas for publishing in maven local.

--- web-canvas/build.gradle.kts
+++ web-canvas/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 plugins {
 	kotlin("js") version "1.3.72"
+	`maven-publish`
 }
 
 dependencies {
@@ -15,4 +16,12 @@ kotlin.target.browser { }
 
 tasks.withType(Kotlin2JsCompile::class) {
 	kotlinOptions.freeCompilerArgs += listOf("-Xopt-in=kotlin.time.ExperimentalTime")
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("web-canvas") {
+			from(components["kotlin"])
+		}
+	}
 }
